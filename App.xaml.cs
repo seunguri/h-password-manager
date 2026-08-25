643e2d9b4a95be170529adfa7b5ea94357c2160c
202501604 <seungahkim@hanwha.com>
2026-08-25 09:11:10 +0900

    realease: profile, version

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Forms;
-using Microsoft.Toolkit.Uwp.Notifications;
 using PasswordProtector.Services;
 using Application = System.Windows.Application;
 
